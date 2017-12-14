6bade82dc2a0917b3defc9dd15f13041783811ea
enriquetecfan <enriquemondejar15@gmail.com>
2017-12-14 17:49:54 +0100

    Conversacion01.py

---
+++ Python/Conversacion01.py
@@ -0,0 +1,10 @@
+#Conversacion en Python
+#Hecho por: Enrique Rodriguez Vela 14/12/17
+#Version 0.1
+#Sistema operativo: Lubuntu
+# IDE Usado : Python 3.5.1 
+#
+
+print ('Hola, ¿Cómo te llamas?')
+nombre=input()
+print('Hola, '+nombre+', ¿Cuantos años tienes?')
