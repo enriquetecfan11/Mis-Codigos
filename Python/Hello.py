d73a23070e24ed780fdafef59ca01fc25082d63c
enriquetecfan <enriquemondejar15@gmail.com>
2017-12-14 17:11:53 +0100

    Python

---
+++ Python/Hello.py
@@ -0,0 +1,11 @@
+# PROGRAMA EN JAVA PARA GUARDAR LAS VARIABLES
+#Hecho por: Enrique Rodriguez Vela 7/11/17
+#Version 0.1
+#Sistema operativo: Lubuntu
+# IDE Usado : Python 2.3 
+#
+
+print("Hello World!")
+print ("Cual es tu nombre")
+myName = input()
+print ("Encantado de conocerte, " + myName)
